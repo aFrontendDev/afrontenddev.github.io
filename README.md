98a1f0ee04982ad7f646b45637d94ddf72e4188b
Andy Blackledge <blackledge22@gmail.com>
2020-03-22 19:18:13 +0000

    ee calc

--- README.md
+++
@@ -1,1 +0,0 @@
-# afrontenddev.github.io
---
+++ precache-manifest.c1cc73e8b57ae1b9b9a634e853551202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed960de8ddeda7b2effac5d40aad6f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de2e074486849855b067",
+    "url": "/static/css/main.e8304351.chunk.css"
+  },
+  {
+    "revision": "752367bf7996f0e0865a",
+    "url": "/static/js/2.7db4a4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7db4a4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2e074486849855b067",
+    "url": "/static/js/main.73f53058.chunk.js"
+  },
+  {
+    "revision": "125bc41114772ba87ac7",
+    "url": "/static/js/runtime-main.d9b47620.js"
+  }
+]);
